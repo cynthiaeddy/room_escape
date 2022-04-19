51355d1860f2a27842903632195d7c828e8b7b76
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-04-19 13:57:58 -0400

    add rotate to landscape for mobile

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Door from './components/Door'
 
 function App() {
   return (
-    <div className="container">
-      <Route exact path="/" component={Opener} />
-      <Route path="/escape" component={Door} />
-    </div>
+    <>
+      <div id="turn">
+        <div className="rotate-container">Please rotate your device</div>
+      </div>
+      <div id="container">
+        <Route exact path="/" component={Opener} />
+        <Route path="/escape" component={Door} />
+      </div>
+    </>
   )
 }
 
