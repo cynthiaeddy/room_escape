92c946005b133a1ce9b7099cf0298935c4d96950
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-04-19 12:09:18 -0400

    position absolute on inner containers

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Door from './components/Door'
 function App() {
   return (
     <>
-      <div id="turn">Please rotate your device!</div>
+      <div id="turn">
+        <div className="rotate-container">Please rotate your device</div>
+      </div>
       <div id="container">
         <Route exact path="/" component={Opener} />
         <Route path="/escape" component={Door} />
